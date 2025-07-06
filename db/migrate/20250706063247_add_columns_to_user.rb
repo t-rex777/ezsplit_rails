ae7c9a8c67a010ff33c59edd412c0a6e508916a3
Manish Kumar Sahu <msahu300@gmail.com>
2025-07-06 13:29:29 +0530

    feat: scaffold with basic auth

---
+++ db/migrate/20250706063247_add_columns_to_user.rb
@@ -0,0 +1,9 @@
+class AddColumnsToUser < ActiveRecord::Migration[8.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :phone, :string
+    add_column :users, :avatar_url, :string
+    add_column :users, :date_of_birth, :date
+  end
+end
