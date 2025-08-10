a71546f6144eb584d0a18c43a6250542d5ddd73c
Manish Kumar Sahu <msahu300@gmail.com>
2025-08-10 10:55:34 +0530

    feat: added user search

--- app/serializers/expense_serializer.rb
+++ app/serializers/expense_serializer.rb
@@ -1,6 +1,6 @@
 class ExpenseSerializer
   include JSONAPI::Serializer
-  attributes :name, :amount, :split_type, :currency, :expense_date, :settled
+  attributes :name, :amount, :split_type, :currency, :expense_date, :settled, :created_at, :updated_at
   belongs_to :payer, serializer: UserSerializer
   belongs_to :group, serializer: GroupSerializer
   belongs_to :category, serializer: CategorySerializer
