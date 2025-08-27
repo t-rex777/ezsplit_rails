74391748e3d7c67e6bccb5c3f3a157f7e2763415
Manish Kumar Sahu <msahu300@gmail.com>
2025-08-27 18:27:59 +0530

    feat: refactor groups management to associate groups with users, update serializers, and enhance request specs for user-specific group access

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,7 +3,7 @@ class GroupsController < ApplicationController
 
   # GET /groups
   def index
-    pagy_obj, @groups = pagy(Group.all)
+    pagy_obj, @groups = pagy(current_user.groups)
     options = {
       include: [ :users ]
     }
@@ -15,33 +15,18 @@ class GroupsController < ApplicationController
      render json: GroupSerializer.new(@group, include: [ :users ]).serializable_hash.to_json
   end
 
-  # GET /groups/new
-  def new
-    @group = Group.new
-  end
-
-  # GET /groups/1/edit
-  def edit
-  end
-
   # POST /groups
   def create
-    @group = Group.new(group_params)
+    @group = current_user.groups.new(group_params)
 
-    respond_to do |format|
       if @group.save
-        format.json do
           render json: GroupSerializer.new(@group).serializable_hash.to_json,
-                 status: :created
-        end
+          status: :created
       else
-        format.json do
           render json: {
             errors: @group.errors.full_messages
           }, status: :unprocessable_entity
-        end
       end
-    end
   end
 
   # PATCH/PUT /groups/1
@@ -51,26 +36,23 @@ class GroupsController < ApplicationController
         if params[:group][:user_ids].present?
           update_members(params[:group][:user_ids])
         end
-
           render json: GroupSerializer.new(@group, include: [ :users ]).serializable_hash.to_json
       else
-        format.json do
           render json: {
             errors: @group.errors.full_messages
           }, status: :unprocessable_entity
-        end
       end
   end
 
   # DELETE /groups/1
   def destroy
-    @group.destroy!
-
-    respond_to do |format|
-      format.json do
+    if @group.destroy! do
         render json: { message: "Group was successfully deleted" }, status: :ok
       end
-      format.html { redirect_to groups_path, status: :see_other, notice: "Group was successfully destroyed." }
+    else
+      render json: {
+        errors: @group.errors.full_messages
+      }, status: :unprocessable_entity
     end
   end
 
@@ -99,20 +81,15 @@ class GroupsController < ApplicationController
 
   private
     def set_group
-      @group = Group.find(params.fetch(:id))
+      @group = current_user.groups.find(params.fetch(:id))
     rescue ActiveRecord::RecordNotFound
-      respond_to do |format|
-        format.json do
           render json: {
             errors: [ "Group not found" ]
           }, status: :not_found
-        end
-        format.html { redirect_to groups_path, alert: "Group not found" }
-      end
     end
 
     def group_params
-      params.require(:group).permit(:name, :description, :created_by_id, user_ids: [])
+      params.require(:group).permit(:name, :description, user_ids: [])
     end
 
     def update_members(user_ids)
--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,9 +1,8 @@
 class GroupSerializer
   include JSONAPI::Serializer
 
-  attributes :id, :name, :description, :created_by_id
+  attributes :name, :description
 
   has_many :group_memberships
   has_many :users, through: :group_memberships
-  belongs_to :created_by, serializer: :user
 end
