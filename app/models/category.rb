35590fd543638e731eeb886e42831ce217d68e7a
Manish Kumar Sahu <msahu300@gmail.com>
2025-08-27 17:30:29 +0530

    feat: refactor category management to associate categories with users, update serializers, and enhance request specs for user-specific category access

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
-  validates :name, :created_by_id, presence: true
-  belongs_to :created_by, class_name: "User", foreign_key: "created_by_id"
+  validates :name, presence: true
+  belongs_to :user
 end
